c35dc36383240b9ba087327f711e47a87844488b
Daris Jorge <it-outtasking16@grupopurdy.com>
2025-05-09 09:40:43 -0600

    Fix tag manager tag

--- Readme/generate-static-pages.js
+++ Readme/generate-static-pages.js
@@ -52,7 +52,7 @@ const generateHtmlContent = (page) => `
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NCJDSW6');</script>
+    })(window,document,'script','dataLayer','GTM-WSRVH3LK');</script>
     <!-- End Google Tag Manager -->
 
     <!-- Metadatos personalizados -->
