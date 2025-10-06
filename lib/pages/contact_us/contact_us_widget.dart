b572f9e31f42c32674e138dfa324b7794b831b9c
Daris Jorge <it-outtasking16@grupopurdy.com>
2025-10-06 16:22:53 -0600

    Update address text

--- lib/pages/contact_us/contact_us_widget.dart
+++ lib/pages/contact_us/contact_us_widget.dart
@@ -1334,7 +1334,7 @@ class _ContactUsWidgetState extends State<ContactUsWidget> {
                                                               'https://maps.app.goo.gl/YwF9cAWcii26m1nQ9');
                                                         },
                                                         child: Text(
-                                                          '400 metros al este de la rotonda Juan Pablo Segundo',
+                                                          '200 mts al sur de la rotonda Juan Pablo II en La Uruca, contiguo al edificio de Intcomex',
                                                           textAlign:
                                                               TextAlign.center,
                                                           style: FlutterFlowTheme
