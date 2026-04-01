8ac6f3b335477f4d597fd817642f480fea3ce50b
Daris Jorge <it-outtasking16@grupopurdy.com>
2026-04-01 16:21:42 -0600

    Update nav bar

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -270,6 +270,7 @@ class FFRoute {
           return transitionInfo.hasTransition
               ? CustomTransitionPage(
                   key: state.pageKey,
+                  name: state.name,
                   child: child,
                   transitionDuration: transitionInfo.duration,
                   transitionsBuilder:
@@ -287,7 +288,8 @@ class FFRoute {
                     child,
                   ),
                 )
-              : MaterialPage(key: state.pageKey, child: child);
+              : MaterialPage(
+                  key: state.pageKey, name: state.name, child: child);
         },
         routes: routes,
       );
--- lib/pages/contact_us/contact_us_widget.dart
+++ lib/pages/contact_us/contact_us_widget.dart
@@ -164,24 +164,21 @@ class _ContactUsWidgetState extends State<ContactUsWidget> {
                     phone: false,
                     tablet: false,
                     tabletLandscape: false,
+                    desktop: false,
                   ))
                     wrapWithModel(
                       model: _model.navBarv2Model,
                       updateCallback: () => safeSetState(() {}),
                       child: NavBarv2Widget(),
                     ),
-                  if (responsiveVisibility(
-                    context: context,
-                    desktop: false,
-                  ))
-                    Container(
-                      decoration: BoxDecoration(),
-                      child: wrapWithModel(
-                        model: _model.navBarMobv2Model,
-                        updateCallback: () => safeSetState(() {}),
-                        child: NavBarMobv2Widget(),
-                      ),
+                  Container(
+                    decoration: BoxDecoration(),
+                    child: wrapWithModel(
+                      model: _model.navBarMobv2Model,
+                      updateCallback: () => safeSetState(() {}),
+                      child: NavBarMobv2Widget(),
                     ),
+                  ),
                   Expanded(
                     child: SingleChildScrollView(
                       child: Column(
