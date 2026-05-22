3b2d81d8c4d430a73ce7affb22745e4c285c7d66
Daris Jorge <it-outtasking16@grupopurdy.com>
2026-05-22 14:18:02 -0600

    Restore function

--- lib/flutter_flow/custom_functions.dart
+++ lib/flutter_flow/custom_functions.dart
@@ -58,7 +58,7 @@ List<BankStruct> filterBanksList(
 }
 
 dynamic convertBanksToJson(List<BankStruct> banks) {
-  List<dynamic> formattedList = banks.map((bank) {
+  return banks.map((bank) {
     return {
       "Nombre": bank.name,
       "Tasa": bank.tasa,
@@ -70,6 +70,4 @@ dynamic convertBanksToJson(List<BankStruct> banks) {
       "fixedRateMonths": bank.fixedRateMonths,
     };
   }).toList();
-  // Convert the list of maps to JSON
-  return jsonEncode(formattedList);
 }
